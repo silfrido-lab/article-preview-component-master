3e199506e38dc5e40c3f01730a8effab3edbf781
Silfrido Benitez <silfrido123chiro@gmail.com>
2020-07-08 22:31:25 +0100

    finish mobil

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ const boton = document.querySelector('#boton');
 boton.addEventListener('click', function() {
     console.log('click');
     document.getElementById('redes').classList.toggle('active');
+    document.getElementById('img').classList.toggle('active');
+    document.getElementById('img-p').classList.toggle('active');
+    document.getElementById('boton').classList.toggle('active');
 });
